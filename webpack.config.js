0246fd42b31f0ed8a0fd4963a10959d21e82a72f
Zaytsev Sasha <zaytsev126@gmail.com>
2018-02-12 15:01:22 +0200

    Setup webpack with babel

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const PATH = {
+    src: path.resolve(__dirname, 'src'),
+    public: path.resolve(__dirname, 'public')
+};
+
+const CONFIG = {
+    context: PATH.src,
+    entry: [
+        './index.js',
+        './index.css'
+    ],
+    output: {
+        path: PATH.public,
+        publicPath: '/',
+        filename: 'bundle.[name].js'
+    },
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new ExtractTextPlugin({
+            filename: 'style.css'
+        }),
+        new HTMLPlugin({
+            filename: 'index.html',
+            template: './index.html',
+            favicon: './images/favicon.ico'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: PATH.src,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }, {
+                test: /.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                })
+            }
+        ]
+    }
+};
+
+module.exports = CONFIG;
