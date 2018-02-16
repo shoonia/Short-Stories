08e9d9ed5cbd5b8b60518142462f341e6e8173fa
Zaytsev Sasha <zaytsev126@gmail.com>
2018-02-16 23:13:44 +0200

    Delete bundle.app

--- public/bundle.not-found.js
+++ public/bundle.not-found.js
@@ -1,1 +1,1 @@
-webpackJsonp([2],{113:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var a=t(0),c=t.n(a),o=function(){return c.a.createElement("div",{className:"container"},"404: Not Found!")};n.default=o}});
+webpackJsonp([1],{112:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var a=t(0),c=t.n(a),o=function(){return c.a.createElement("div",{className:"container"},"404: Not Found!")};n.default=o}});
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import Loadable from 'react-loadable';
 import { render } from 'react-dom';
 
-import Loader from './components/loader/Loader.jsx';
-
-const App = Loadable({
-    loader: () => import('./components/App.jsx' /* webpackChunkName: "app" */),
-    loading: Loader
-});
+import App from './components/App.jsx';
 
 render(
     React.createElement(App),
